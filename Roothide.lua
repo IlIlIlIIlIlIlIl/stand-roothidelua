94669123ae5ed3350559cfb1be7e8fe0d112a80a
IlIlIlIIlIlIlIl <107168271+IlIlIlIIlIlIlIl@users.noreply.github.com>
2024-05-10 19:21:19 +1000

    Update Roothide.lua

--- Roothide.lua
+++ Roothide.lua
@@ -5,7 +5,7 @@ util.ensure_package_is_installed("lua/auto-updater")
 local auto_updater = require("auto-updater")
 
 auto_updater.run_auto_update({
-    source_url="URLHERE",
+    source_url="https://raw.githubusercontent.com/IlIlIlIIlIlIlIl/roothidelua-stand/main/Roothide.lua",
     script_relpath=SCRIPT_RELPATH
 })
 
